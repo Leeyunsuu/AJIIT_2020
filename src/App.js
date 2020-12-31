5b25c51242adcd4d55d44eb0d065b27eae8a755a
yunsuteng <75407987+Leeyunsuu@users.noreply.github.com>
2020-12-31 23:30:51 +0900

    2020/12/31/23:30
    Component Life Style 복습

--- src/App.js
+++ src/App.js
@@ -3,18 +3,32 @@ import PropTypes from "prop-types";
 
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    console.log("Hello");
+  };
   state ={
     count: 0
   };
   plus = () => {
     console.log("plus");
-    this.setState(current => ({count: current.count +1}));
+    this.setState(current => ({count: current.count +1})); //Component update
   };
   minus = () => {
     console.log("minus");
-    this.setState(current => ({count: current.count +1}));
+    this.setState(current => ({count: current.count -1}));
   };
+  componentDidMount(){ //Compiled하고 마지막에
+    console.log("component rendered");
+  }
+  componentDidUpdate(){  //Component가 Update된 후
+    console.log("component did update")
+  }
+  componentWillUnmount(){ //Component가 Unmount 후
+    console.log("Component die!");
+  }
   render() {
+    console.log("render");
     return (
     <div>
       <h1> Class Component 입니다.{this.state.count}</h1>
